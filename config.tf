485c6ae14ef1c454b1641fa39fe0c70a88c94a8f
Steve Borosh <rvrsh3ll@users.noreply.github.com>
2021-06-20 14:27:35 -0400

    Update
    Removed CDN, domain fronting parts

--- config.tf
+++ config.tf
@@ -1,38 +1,25 @@
 #Azure location for deployment
-variable "location"    { default = "westeurope"}
-
-//Public SSH key to be placed on the teamserver VM
-variable "ssh_key"    { default = "<PUBLIC SSH KEY>"}
+variable "location"    { default = "eastus"}
 
 //Your profile name , must be placed in Ressources/ (Just the name here, not path!)
 variable "PROFILE_FILE"    { default = "jquery-c2.4.2.profile" }
 
-#Use a long password instead of special chars, migh break bash command
-variable "TEAM_PASS"    { default = "<TEAM SERVER PASSWORD>" }
-
-#Serial key for installing cobalt teamserver
-variable "COBALTKEY"    { default = "<COBALT SERIAL KEY>" }
-
 #Azure function name without <.azurewebsites.net>
 variable "func_name"    { default = "<AZURE FUNC NAME>" }
 
-#CDN Endpoint name without <.azureedge.net>
-variable "cdn_endpoint_name"    { default = "<CDN ENDPOINT NAME>" }
-
 #Site to redirect traffic to that dosent not match mall profile
 variable "decoy_website"    { default = "https://microsoft.com" }
+
+# IP Address of your C2 Server
+variable "ts_ip" { default = "<IP ADDRESS>"}
 
 #MUST BE REPLACED TO AVOID COLISSION WITH OTHER AZURE GLOBAL RESSOURCES 
 variable "uniq_prefix"    { default = "<RANDOM 4 LETTERS>" }
 
 #No need to edit anything below this line!
-variable "cdn_profile_name"    { default = "relay-cdn-profile"}
 variable "resource_group"    { default = "relay-rg"}
-variable "vm_name"    { default = "relay-teamserver-vm"}
 variable "plan_name"    { default = "relay-appsvc"}
 variable "stor_name"    { default = "relaydata001"}
-variable "vm_username"    { default = "azureuser" }
-variable "vm_diskname"    { default = "relayteamservervmsto0" }
 variable "nic_name"    { default = "relay-1nic" }
 variable "public_ip_name"    { default = "relay-pia" }
 variable "nic_sec_name"    { default = "relay-nsg" }
@@ -42,4 +29,3 @@ variable "vnet_name"    { default = "relay-vnet" }
 variable "ai_name"    { default = "appai" }
 
 variable "func_zip_path"    { default = "AzureC2Relay.zip" }
-
--- deploy.tf
+++ deploy.tf
@@ -8,7 +8,6 @@ terraform {
   }
 }
 
-
 provider "azurerm" {
   features {}
 }
@@ -16,21 +15,6 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "${var.uniq_prefix}${var.resource_group}"
   location = "${var.location}"
-}
-
-
-resource "azurerm_resource_group_template_deployment" "azure_cdn" {
-    name                = "azurecdn"
-    resource_group_name = azurerm_resource_group.rg.name
-    deployment_mode     = "Incremental"
-    template_content    = file("arm_template.json")
- 
-    parameters_content =  jsonencode({
-        cdn_profile_name              = {value =  "${var.uniq_prefix}${var.cdn_profile_name}"}
-        endpoint_name                 = {value =  "${var.cdn_endpoint_name}"}
-        app_func_name                 = {value =  "${var.func_name}.azurewebsites.net"}
-        location                      = {value =  "${var.location}"}
-    })  
 }
 
 
@@ -68,7 +52,7 @@ resource "azurerm_function_app" "func" {
     FUNCTIONS_WORKER_RUNTIME                    = "dotnet"
     SCM_DO_BUILD_DURING_DEPLOYMENT              = true
     MalleableProfileB64                          = "${data.local_file.ParseMalleable.content}"
-    RealC2EndPoint								= "https://${azurerm_linux_virtual_machine.vm.private_ip_address}:443/"
+    RealC2EndPoint								= "https://${var.ts_ip}:443/"
   	DecoyRedirect								= "${var.decoy_website}"
 	APPINSIGHTS_INSTRUMENTATIONKEY				= "${azurerm_application_insights.funcai.instrumentation_key}"
     
@@ -79,7 +63,6 @@ resource "azurerm_function_app" "func" {
 	}
 
    depends_on = [
-    azurerm_linux_virtual_machine.vm,
     data.local_file.ParseMalleable,
 	azurerm_application_insights.funcai
   ]
@@ -116,7 +99,6 @@ resource "azurerm_subnet" "subnet1" {
 
 }
 
-
 resource "azurerm_subnet" "subnet2" {
   name                 = "${var.uniq_prefix}${var.subnet_two_name}"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -145,144 +127,12 @@ resource "azurerm_public_ip" "pubip" {
   allocation_method   = "Static"
 
   }
-  
-  resource "azurerm_network_security_group" "nicsec" {
-  name                = "${var.uniq_prefix}${var.nic_sec_name}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.uniq_prefix}${var.nic_name}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet1.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pubip.id
-  }
- depends_on = [
-   azurerm_subnet.subnet1,
-   ]
-
-}
-
-
-resource "azurerm_network_interface_security_group_association" "nicass" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nicsec.id
-
- depends_on = [
-        azurerm_network_interface.nic,
-  ]
-
-}
-
-
-resource "tls_private_key" "public_private_key_pair" {
-  algorithm   = "RSA"
-}
-
-
-# Create virtual machine
-resource "azurerm_linux_virtual_machine" "vm" {
-    name                  = "${var.uniq_prefix}${var.vm_name}"
-    location              = azurerm_resource_group.rg.location
-    resource_group_name   = azurerm_resource_group.rg.name
-    network_interface_ids = [azurerm_network_interface.nic.id]
-    size                  = "Standard_DS1_v2"
-
-    os_disk {
-        name              = "${var.uniq_prefix}${var.vm_diskname}"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
-
-    computer_name  = "teamservervm"
-    admin_username = "${var.vm_username}"
-    disable_password_authentication = true
-
-    admin_ssh_key {
-	    username   = "${var.vm_username}"
-	    public_key = tls_private_key.public_private_key_pair.public_key_openssh
-    }
-	
-	provisioner "file" {
-	  source      = "Ressources/"
-	  destination = "/home/${var.vm_username}/"
-
-	  connection {
-		type        = "ssh"
-		user        = "${var.vm_username}"
-		private_key =  tls_private_key.public_private_key_pair.private_key_pem
-		timeout     = "2m"
-		host        = "${self.public_ip_address}"
-	  }
-	}
-
-  provisioner "remote-exec" {
- 
-	  inline = [
-      "echo \"${var.ssh_key}\" >> ~/.ssh/authorized_keys",
-      "sudo apt update -y",
-      "sudo apt -f install openjdk-11-jre-headless -y",
-	  "sudo apt -f install openjdk-11-jdk-headless -y",
-      "tar xvf cobaltstrike-dist.tgz",
-      "rm cobaltstrike-dist.tgz",
-      "cd cobaltstrike", 
-      "echo \"${var.COBALTKEY}\" | bash update", 
-      "cp ~/${var.PROFILE_FILE} ~/cobaltstrike/${var.PROFILE_FILE}",
-      "echo \"sudo bash teamserver ${self.private_ip_address} ${var.TEAM_PASS} ${var.PROFILE_FILE}\" > RunServer.sh",
-      "tmux new-session -d -s CobaltTeamServer 'sudo bash RunServer.sh'",
-      ]
-
-	  connection {
-		type        = "ssh"
-		user        = "${var.vm_username}"
-		private_key =  tls_private_key.public_private_key_pair.private_key_pem
-		timeout     = "10m"
-		host        = "${self.public_ip_address}"
-	  }
-	}
-
-	
-   depends_on = [
-	  azurerm_network_interface_security_group_association.nicass,
-   
-  ]
-
-}
-
 
 resource "time_sleep" "wait_60_seconds" {
   depends_on = [null_resource.funcrestart]
 
   create_duration = "60s"
 }
-
-
 
 resource "null_resource" "funcrestart" {
 
@@ -317,8 +167,6 @@ resource "azurerm_app_service_virtual_network_swift_connection" "funcass" {
    ]
 }
 
-
-
 resource "null_resource" "shell" {
   provisioner "local-exec" {
     command = "dotnet ParseMalleable/ParseMalleable.dll Ressources/${var.PROFILE_FILE} > ParsedMalleableData.txt"
@@ -329,26 +177,4 @@ resource "null_resource" "shell" {
 data "local_file" "ParseMalleable" {
 		filename = "ParsedMalleableData.txt"	
 		depends_on = [null_resource.shell,]
-}
-
-
-
-#Output SSH data for interaction with VM
-output "ssh_vm" {
-  value = "ssh -L 50050:localhost:50050 ${var.vm_username}@${azurerm_linux_virtual_machine.vm.public_ip_address}"
-
-  depends_on = [
-   null_resource.funcdep,
-   ]
-
-}
-
-#Output CDN endpoint name
-output "cdn_endpoint" {
-  value = "${var.cdn_endpoint_name}.azureedge.net"
-
-  depends_on = [
-    azurerm_resource_group_template_deployment.azure_cdn,
-   ]
-
 }
